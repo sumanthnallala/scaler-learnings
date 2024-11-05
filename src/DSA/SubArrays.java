4346a7608c84a15d6b4d3ab5da6540f10a013022
Sumanth Nallala <nallala.sumanthreddy111@gmail.com>
2024-11-05 12:31:54 +0530

    Subsequence count

--- src/DSA/SubArrays.java
+++ src/DSA/SubArrays.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class SubArrays {
 
   public static void main(String[] args) {
-    findSubArrays();
+    int[] arr = {1, 2, 3};
+    //    findSubArrays();
+    sumOfPossibleSubArrays();
   }
 
   public static void findSubArrays() {
@@ -22,6 +24,29 @@ public class SubArrays {
         array = new ArrayList<>();
       }
       array = new ArrayList<>();
+    }
+  }
+
+  public static int[] calculatePrefixSum(int[] A) {
+    int n = A.length;
+    int[] prefixSumArray = new int[n];
+    prefixSumArray[0] = A[0];
+    for (int i = 1; i < n; i++) {
+      prefixSumArray[i] = A[i] + prefixSumArray[i - 1];
+    }
+    return prefixSumArray;
+  }
+
+  public static void sumOfPossibleSubArrays() {
+    // optimized approach with space as O(1) and time as O(N2) from O(N) and O(N3)
+    int[] arr = {1, 2, 3};
+    int n = arr.length;
+    for (int i = 0; i < n; i++) {
+      int sum = 0;
+      for (int j = i; j < n; j++) {
+        sum += arr[j];
+        System.out.println(sum);
+      }
     }
   }
 }
