344175c6911d3103bea5da57f9c40d7eef6856b4
Sumanth Nallala <nallala.sumanthreddy111@gmail.com>
2024-11-06 12:23:29 +0530

    Subsequence count

--- src/DSA/TwoDimensionalArray.java
+++ src/DSA/TwoDimensionalArray.java
@@ -7,9 +7,11 @@ public class TwoDimensionalArray {
   public static void main(String[] args) {
     rowWiseSum();
     columnWiseSum();
+    diagonalSum();
+    antiDiagonalSum();
   }
 
-  public static int[][] declareAndInitialize() {
+  public static int[][] declareAndInitialize2DArray() {
     // 1st way
     int[][] arr1 = {{1, 2, 3}, {1, 2, 3}};
 
@@ -19,7 +21,7 @@ public class TwoDimensionalArray {
     int columns = arr[0].length;
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
-        arr[i][j] = i + j;
+        arr[i][j] = i * j;
       }
     }
     return arr;
@@ -27,31 +29,56 @@ public class TwoDimensionalArray {
 
   public static void rowWiseSum() {
     // calculate the row wise sum for a 2D Matrix
-    int[][] arr = declareAndInitialize();
+    int[][] arr = declareAndInitialize2DArray();
     int r = arr.length;
     int c = arr[0].length;
-    for(int i = 0; i < r; i++) {
+    for (int i = 0; i < r; i++) {
       int sum = 0;
-      for(int j = 0; j < c; j++) {
+      for (int j = 0; j < c; j++) {
         sum += arr[i][j];
       }
-      System.out.printf("sum of the row %d is %d\n", i ,sum);
+      System.out.printf("sum of the row %d is %d\n", i, sum);
     }
     System.out.println();
   }
 
   public static void columnWiseSum() {
     // calculate the column wise sum for a 2D Matrix
-    int[][] arr = declareAndInitialize();
+    int[][] arr = declareAndInitialize2DArray();
     int r = arr.length;
     int c = arr[0].length;
-    for(int i = 0; i < c; i++) {
+    for (int i = 0; i < c; i++) {
       int sum = 0;
-      for(int j = 0; j < r; j++) {
+      for (int j = 0; j < r; j++) {
         sum += arr[j][i];
       }
-      System.out.printf("sum of the column %d is %d\n", i ,sum);
+      System.out.printf("sum of the column %d is %d\n", i, sum);
     }
   }
 
+  public static void diagonalSum() {
+    int[][] arr = declareAndInitialize2DArray();
+    System.out.println(Arrays.deepToString(arr));
+    int r = arr.length;
+    int c = arr[0].length;
+    int n = Math.min(r, c);
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+      total += arr[i][i];
+    }
+    System.out.printf("Diagonal sum of the matrix is: %d\n", total);
+  }
+
+  public static void antiDiagonalSum() {
+    int[][] arr = declareAndInitialize2DArray();
+    System.out.println(Arrays.deepToString(arr));
+    int r = arr.length;
+    int c = arr[0].length;
+    int n = Math.min(r, c);
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+      total += arr[i][n - i - 1];
+    }
+    System.out.printf("Anti Diagonal sum of the matrix is: %d\n", total);
+  }
 }
