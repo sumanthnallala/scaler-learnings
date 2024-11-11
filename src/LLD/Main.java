933e9eb5f2f448e1d79795011ee640e1d571f93e
Sumanth Nallala <nallala.sumanthreddy111@gmail.com>
2024-11-11 17:51:28 +0530

    Thread

--- src/LLD/Main.java
+++ src/LLD/Main.java
@@ -1,10 +1,17 @@
 package LLD;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Main {
   public static void main(String[] args) {
     Threads threads = new Threads();
     Thread t = new Thread(threads);
     t.start();
     System.out.println("Hello world from thread main: " + Thread.currentThread().getName());
+    Executor executor = Executors.newFixedThreadPool(10);
+    // twice the number of cores should be the threads
+    System.out.println("Sumanth");
+    executor.execute(threads);
   }
 }
